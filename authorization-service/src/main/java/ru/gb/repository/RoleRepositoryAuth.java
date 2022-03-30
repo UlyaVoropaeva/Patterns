a0300df0bc782c7af5d6eb6fcd92af20fdcb5e9d
UlyaVoropaeva <79697027+UlyaVoropaeva@users.noreply.github.com>
2022-03-30 22:22:54 +0300

    Delete authorization-service/src/main directory

--- authorization-service/src/main/java/ru/gb/repository/RoleRepositoryAuth.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.gb.entity.Role;
-
-public interface RoleRepositoryAuth extends CrudRepository<Role, Long> {
-
-}
